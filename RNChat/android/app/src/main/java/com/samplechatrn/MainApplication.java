beda5bfc06e29c89bca4923eb7c018483b1a868a
gilons <gf.694765457@gmail.com>
2019-09-15 16:31:22 +0100

    react-native rundroid not working issue resolved

--- RNChat/android/app/src/main/java/com/samplechatrn/MainApplication.java
+++ RNChat/android/app/src/main/java/com/samplechatrn/MainApplication.java
@@ -3,10 +3,15 @@ package com.samplechatrn;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import io.github.mr03web.softinputmode.SoftInputModePackage;
+import com.wix.autogrowtextinput.AutoGrowTextInputPackage;
+import com.reactnativecommunity.netinfo.NetInfoPackage;
+import com.RNFetchBlob.RNFetchBlobPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
-import io.github.mr03web.softinputmodemodule.SoftInputModePackage;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -27,10 +32,15 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNGestureHandlerPackage(),
+            new SoftInputModePackage(),
+            new AutoGrowTextInputPackage(),
+            new NetInfoPackage(),
           new RNDeviceInfo(),
+          new RNFetchBlobPackage(),
+          new AsyncStoragePackage(),
           new ReactNativePushNotificationPackage(),
-          new VectorIconsPackage(),
-          new SoftInputModePackage()
+          new VectorIconsPackage()
       );
     }
 
