d45d050edf729fb011ca5f23deca284fc3cafd1d
Vlad <vlad@connectycube.com>
2019-10-01 01:52:34 +0300

    fixes android MainActivity

--- RNVideoChat/android/app/src/main/java/com/rnvideochat/AppMainActivity.java
+++ RNVideoChat/android/app/src/main/java/com/rnvideochat/AppMainActivity.java
@@ -2,7 +2,7 @@ package com.rnvideochat;
 
 import com.facebook.react.ReactActivity;
 
-public class MainActivity extends ReactActivity {
+public class AppMainActivity extends ReactActivity {
 
   @Override
   protected String getMainComponentName() {
--- RNVideoChat/src/services/CallingService.js
+++ RNVideoChat/src/services/CallingService.js
@@ -1,127 +1,116 @@
-import ConnectyCube from 'connectycube-reactnative'
-import { Alert, Platform } from 'react-native'
+import ConnectyCube from 'connectycube-reactnative';
+import {Alert, Platform} from 'react-native';
 
 class CallingService {
-	getUserMedia(session) {
-		return new Promise((resolve, reject) => {
-			session.getUserMedia({
-				audio: true,
-				video: { facingMode: 'user' }
-			}, function(error, stream) {
-				error ? reject(error) : resolve(stream);
-			});
-		});
-	}
+  getUserMedia(session) {
+    return new Promise((resolve, reject) => {
+      session.getUserMedia(
+        {
+          audio: true,
+          video: {facingMode: 'user'},
+        },
+        function(error, stream) {
+          error ? reject(error) : resolve(stream);
+        },
+      );
+    });
+  }
 
-	switchCamera(localStream) {
-		// MediaStreamTrack.prototype._switchCamera()
-		// switch the front/back cameras in a video track on the fly, without the need for adding/removing tracks or renegotiating
-		localStream.getVideoTracks().forEach(track => track._switchCamera());
-	}
+  switchCamera(localStream) {
+    // MediaStreamTrack.prototype._switchCamera()
+    // switch the front/back cameras in a video track on the fly, without the need for adding/removing tracks or renegotiating
+    localStream.getVideoTracks().forEach(track => track._switchCamera());
+  }
 
-	getVideoDevices() {
-		return new Promise((resolve, reject) => {
-			ConnectyCube.videochat.getMediaDevices('videoinput').then(devices => {
-				devices ? resolve(devices) : reject();
-			});
-		});
-	}
+  getVideoDevices() {
+    return new Promise((resolve, reject) => {
+      ConnectyCube.videochat.getMediaDevices().then(devices => {
+        console.warn(devices);
+        devices ? resolve(devices) : reject();
+      });
+    });
+  }
 
-	createVideoSession(calleesIds) {
-		return new Promise((resolve, reject) => {
-			const sessionType = ConnectyCube.videochat.CallType.VIDEO; // AUDIO is also possible
-			const additionalOptions = {};
-			const session = ConnectyCube.videochat.createNewSession(calleesIds, sessionType, additionalOptions);
-			resolve(session)
-		});
-	}
+  createVideoSession(calleesIds) {
+    return new Promise((resolve, reject) => {
+      const sessionType = ConnectyCube.videochat.CallType.VIDEO; // AUDIO is also possible
+      const additionalOptions = {};
+      const session = ConnectyCube.videochat.createNewSession(
+        calleesIds,
+        sessionType,
+        additionalOptions,
+      );
+      resolve(session);
+    });
+  }
 
-	initiateCall(session) {
-		var extension = {filter: "0"};
-		session.call(extension, function(error) {
+  initiateCall(session) {
+    var extension = {filter: '0'};
+    session.call(extension, function(error) {});
+  }
 
-		});
-	}
+  acceptCall(session) {
+    var extension = {};
+    session.accept(extension);
+  }
 
-	acceptCall(session) {
-		var extension = {};
-		session.accept(extension);
-	}
+  rejectCall(session) {
+    var extension = {};
+    session.reject(extension);
+  }
 
-	rejectCall(session) {
-		var extension = {};
-		session.reject(extension);
-	}
+  finishCall(session) {
+    var extension = {};
+    session.stop(extension);
 
-	finishCall(session) {
-		var extension = {};
-		session.stop(extension);
+    ConnectyCube.videochat.clearSession(session.ID);
+  }
 
-		ConnectyCube.videochat.clearSession(session.ID);
-	}
+  muteAudio(session) {
+    session.mute('audio');
+  }
 
-	muteAudio(session) {
-		session.mute('audio');
-	}
+  unmuteAudio(session) {
+    session.unmute('audio');
+  }
 
-	unmuteAudio(session) {
-		session.unmute('audio');
-	}
+  processOnUserNotAnswer(session, userId) {
+    console.log('CallingService processOnUserNotAnswer', userId);
 
-	processOnUserNotAnswer(session, userId) {
-		console.log("CallingService processOnUserNotAnswer", userId);
+    Alert.alert(
+      'An opponent did not answer',
+      '',
+      [{text: 'Ok', onPress: () => {}}],
+      {cancelable: true},
+    );
+  }
 
-		Alert.alert(
-			'An opponent did not answer',
-			'',
-			[
-				{text: 'Ok', onPress: () => {
+  processOnAcceptCallListener(session, extension) {}
 
-				}},
-			],
-			{cancelable: true},
-		);
-	}
+  processOnRejectCallListener(session, extension) {
+    ConnectyCube.videochat.clearSession(session.ID);
 
-	processOnAcceptCallListener(session, extension) {
+    Alert.alert(
+      'An opponent rejected the call request',
+      '',
+      [{text: 'Ok', onPress: () => {}}],
+      {cancelable: true},
+    );
+  }
 
-	}
+  processOnStopCallListener(session, extension) {
+    ConnectyCube.videochat.clearSession(session.ID);
 
-	processOnRejectCallListener(session, extension) {
-		ConnectyCube.videochat.clearSession(session.ID);
-
-		Alert.alert(
-			'An opponent rejected the call request',
-			'',
-			[
-				{text: 'Ok', onPress: () => {
-
-				}},
-			],
-			{cancelable: true},
-		);
-	}
-
-	processOnStopCallListener(session, extension) {
-		ConnectyCube.videochat.clearSession(session.ID);
-
-		Alert.alert(
-			'The call is finished',
-			'',
-			[
-				{text: 'Ok', onPress: () => {
-
-				}},
-			],
-			{cancelable: true},
-		);
-	}
+    Alert.alert('The call is finished', '', [{text: 'Ok', onPress: () => {}}], {
+      cancelable: true,
+    });
+  }
 }
 
 // create instance
-const Calling = new CallingService()
+const Calling = new CallingService();
 
 // lock instance
-Object.freeze(Calling)
+Object.freeze(Calling);
 
-export default Calling
+export default Calling;
