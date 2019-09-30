656782db48c4de4f5910b71082f41ad254b64732
Vlad <vlad@connectycube.com>
2019-09-30 15:50:24 +0300

    RNChat migrated to React Native 0.61.1.

--- RNChat/src/components/Root.js
+++ RNChat/src/components/Root.js
@@ -1,166 +1,170 @@
-import React, { Component } from 'react'
-import { AppState } from 'react-native';
-import { Actions } from 'react-native-router-flux'
-import { connect } from 'react-redux'
-import { userLogin } from '../actions/user'
-import { fetchDialogs, sortDialogs } from '../actions/dialogs'
-import { chatConnected, chatDisconnected } from '../actions/connection'
-import { pushMessage } from '../actions/messages'
-import ConnectyCube from 'connectycube-reactnative'
+import React, {Component} from 'react';
+import {AppState} from 'react-native';
+import {Actions} from 'react-native-router-flux';
+import {connect} from 'react-redux';
+import {userLogin} from '../actions/user';
+import {fetchDialogs, sortDialogs} from '../actions/dialogs';
+import {chatConnected, chatDisconnected} from '../actions/connection';
+import {pushMessage} from '../actions/messages';
+import ConnectyCube from 'connectycube-reactnative';
 import appConfig from '../../app.json';
-import AppRouter from '../router'
-import Message from '../models/Message'
-import User from '../services/UserService'
-import Chat from '../services/ChatService'
+import AppRouter from '../router';
+import Message from '../models/Message';
+import User from '../services/UserService';
+import Chat from '../services/ChatService';
 import PushNotificationService from '../services/PushNotification';
 
 class AppRoot extends Component {
-	constructor(props) {
-		super(props)
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			appIsActive: true,
-			waitConnect: false
-		}
+    AppState.addEventListener('change', this._handleAppStateChange.bind(this));
 
-		this.pushService
-	}
+    ConnectyCube.init(...appConfig.connectyCubeConfig);
 
-	componentWillMount() {
-		AppState.addEventListener('change', this._handleAppStateChange.bind(this))
+    User.autologin()
+      .then(this.props.userLogin)
+      .catch(() => Actions.auth());
 
-		ConnectyCube.init(...appConfig.connectyCubeConfig)
+    this.state = {
+      appIsActive: true,
+      waitConnect: false,
+    };
 
-		User.autologin()
-			.then(this.props.userLogin)
-			.catch(() => Actions.auth())
-	}
+    this.pushService;
+  }
 
   componentWillUnmount() {
-    AppState.removeEventListener('change', this._handleAppStateChange.bind(this));
-	}
+    AppState.removeEventListener(
+      'change',
+      this._handleAppStateChange.bind(this),
+    );
+  }
 
-	componentWillReceiveProps(props) {
-		this._connect(props)
-	}
+  componentWillReceiveProps(props) {
+    this._connect(props);
+  }
 
-	/*               *
-	 * Render method *
-	 *               */
-	render() {
-		return <AppRouter />
-	}
-	
-	/*               *
-	 * Chat activity *
-	 *               */
-	_handleAppStateChange(nextAppState) {
-		if (nextAppState === 'active') {
-			this.setState({ appIsActive: true })
-			this._reconnect()
-		} else {
-			this.setState({ appIsActive: false })
-			this._disconnect()
-		}
-	}
+  /*               *
+   * Render method *
+   *               */
+  render() {
+    return <AppRouter />;
+  }
 
-	_connect(props) {
-		const { connected, user, chatConnected, fetchDialogs } = props
-		const { waitConnect, appIsActive } = this.state
+  /*               *
+   * Chat activity *
+   *               */
+  _handleAppStateChange(nextAppState) {
+    if (nextAppState === 'active') {
+      this.setState({appIsActive: true});
+      this._reconnect();
+    } else {
+      this.setState({appIsActive: false});
+      this._disconnect();
+    }
+  }
 
-		if (appIsActive && user && !connected && !waitConnect) {			
-			this.setState({ waitConnect: true })
+  _connect(props) {
+    const {connected, user, chatConnected, fetchDialogs} = props;
+    const {waitConnect, appIsActive} = this.state;
 
-			Chat.getConversations()
-				.then(dialogs => {
-					Actions.dialogs()
-					Chat.connect(user, dialogs)
-					fetchDialogs(dialogs)
-				})
-				.then(() => {
-					chatConnected()
-					this._setupListeners()
-				})
-				.catch(e => alert(`Error.\n\n${JSON.stringify(e)}`))
-				.then(() => this.setState({ waitConnect: false }))
+    if (appIsActive && user && !connected && !waitConnect) {
+      this.setState({waitConnect: true});
 
-			new PushNotificationService(this.onNotificationListener.bind(this))
-		}
-	}
-	
-	_reconnect() {
-		const { connected, user, chatConnected } = this.props
-		const { waitConnect, appIsActive } = this.state
+      Chat.getConversations()
+        .then(dialogs => {
+          Actions.dialogs();
+          Chat.connect(user, dialogs);
+          fetchDialogs(dialogs);
+        })
+        .then(() => {
+          chatConnected();
+          this._setupListeners();
+        })
+        .catch(e => alert(`Error.\n\n${JSON.stringify(e)}`))
+        .then(() => this.setState({waitConnect: false}));
 
-		if (appIsActive && user && !connected && !waitConnect) {			
-			this.setState({ waitConnect: true })
+      new PushNotificationService(this.onNotificationListener.bind(this));
+    }
+  }
 
-			Chat.getConversations()
-				.then(dialogs => {
-					Chat.connect(user, dialogs)
-					fetchDialogs(dialogs)
-				})
-				.then(chatConnected)
-				.catch(e => alert(`Error.\n\n${JSON.stringify(e)}`))
-				.then(() => this.setState({ waitConnect: false }))
-		}
-	}
+  _reconnect() {
+    const {connected, user, chatConnected} = this.props;
+    const {waitConnect, appIsActive} = this.state;
 
-	_disconnect() {
-		this.props.chatDisconnected()
-		Chat.disonnect()
-	}
+    if (appIsActive && user && !connected && !waitConnect) {
+      this.setState({waitConnect: true});
 
-	_setupListeners() {
-		ConnectyCube.chat.onDisconnectedListener = this.props.chatDisconnected
-		ConnectyCube.chat.onReconnectedListener = this.props.chatDisconnected
-		ConnectyCube.chat.onMessageListener = this._onMessageListener.bind(this)
-		// ConnectyCube.chat.onSentMessageCallback = this._onSentMessage.bind(this)
-	}
+      Chat.getConversations()
+        .then(dialogs => {
+          Chat.connect(user, dialogs);
+          fetchDialogs(dialogs);
+        })
+        .then(chatConnected)
+        .catch(e => alert(`Error.\n\n${JSON.stringify(e)}`))
+        .then(() => this.setState({waitConnect: false}));
+    }
+  }
 
-	_onMessageListener(id, msg) {
-		const { user, selected, pushMessage, sortDialogs } = this.props
-		const message = new Message(msg)
+  _disconnect() {
+    this.props.chatDisconnected();
+    Chat.disonnect();
+  }
 
-		if (id !== user.id) {
-			if (selected.id === message.dialog_id) {
-				pushMessage(message)
-				sortDialogs(message)
-				Chat.readMessage(message.id, message.dialog_id)
-			} else {
-				sortDialogs(message, true)
-			}
-		}
-	}
+  _setupListeners() {
+    ConnectyCube.chat.onDisconnectedListener = this.props.chatDisconnected;
+    ConnectyCube.chat.onReconnectedListener = this.props.chatDisconnected;
+    ConnectyCube.chat.onMessageListener = this._onMessageListener.bind(this);
+    // ConnectyCube.chat.onSentMessageCallback = this._onSentMessage.bind(this)
+  }
 
-	// _onSentMessage(failedMessage, successMessage) {
-	// 	if (failedMessage && !successMessage) {
-	// 		console.log('Send message - FAIL');
-	// 	} else {
-	// 		console.log('Send message - SUCCESS');
-	// 	}
-	// }
+  _onMessageListener(id, msg) {
+    const {user, selected, pushMessage, sortDialogs} = this.props;
+    const message = new Message(msg);
 
-	onNotificationListener(notification) {
-		Actions.dialogs()
-	}
+    if (id !== user.id) {
+      if (selected.id === message.dialog_id) {
+        pushMessage(message);
+        sortDialogs(message);
+        Chat.readMessage(message.id, message.dialog_id);
+      } else {
+        sortDialogs(message, true);
+      }
+    }
+  }
 
+  // _onSentMessage(failedMessage, successMessage) {
+  // 	if (failedMessage && !successMessage) {
+  // 		console.log('Send message - FAIL');
+  // 	} else {
+  // 		console.log('Send message - SUCCESS');
+  // 	}
+  // }
+
+  onNotificationListener(notification) {
+    console.warn(notification);
+    Actions.dialogs();
+  }
 }
 
-const mapStateToProps = (state) => ({
-	connected: state.connection,
-	user: state.user,
-	selected: state.selected,
-	dialogs: state.dialogs
-})
+const mapStateToProps = state => ({
+  connected: state.connection,
+  user: state.user,
+  selected: state.selected,
+  dialogs: state.dialogs,
+});
 
-const mapDispatchToProps = (dispatch) => ({
-	chatConnected: () => dispatch(chatConnected()),
-	chatDisconnected: () => dispatch(chatDisconnected()),
-	userLogin: user => dispatch(userLogin(user)),
-	fetchDialogs: dialogs => dispatch(fetchDialogs(dialogs)),
-	sortDialogs: (message, count) => dispatch(sortDialogs(message, count)),
-	pushMessage: message => dispatch(pushMessage(message))
-})
+const mapDispatchToProps = dispatch => ({
+  chatConnected: () => dispatch(chatConnected()),
+  chatDisconnected: () => dispatch(chatDisconnected()),
+  userLogin: user => dispatch(userLogin(user)),
+  fetchDialogs: dialogs => dispatch(fetchDialogs(dialogs)),
+  sortDialogs: (message, count) => dispatch(sortDialogs(message, count)),
+  pushMessage: message => dispatch(pushMessage(message)),
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(AppRoot)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(AppRoot);
--- RNChat/src/services/PushNotification.js
+++ RNChat/src/services/PushNotification.js
@@ -1,43 +1,50 @@
-import { Platform } from 'react-native';
-import ConnectyCube from 'connectycube-reactnative'
-import DeviceInfo from 'react-native-device-info'
+import {Platform} from 'react-native';
+import ConnectyCube from 'connectycube-reactnative';
 // TODO: to disable fo iOS
 // import PushNotification from 'react-native-push-notification'
 import appConfig from '../../app.json';
 
 // TODO: use import from 'react-native-push-notification' for both OS
 if (Platform.OS === 'android') {
-	var PushNotification = require('react-native-push-notification')
+  var PushNotification = require('react-native-push-notification');
 }
 
 export default class PushNotificationService {
   constructor(onNotification) {
-    this.init(onNotification)
+    this.init(onNotification);
   }
 
   init(onNotification) {
-    if (Platform.OS === 'ios') return
+    if (Platform.OS === 'ios') return;
 
     PushNotification.configure({
       onRegister: this.subscribe,
       onNotification: onNotification,
-      senderID: appConfig.senderID
-    })
+      senderID: appConfig.senderID,
+    });
   }
-  
+
   subscribe(register) {
+    const DeviceInfo = require('react-native-device-info').default;
+    console.warn('register', register);
+    console.warn('DeviceInfo', DeviceInfo.getUniqueID());
     const params = {
       notification_channels: Platform.OS === 'ios' ? 'apns' : 'gcm',
       device: {
         platform: Platform.OS,
-        udid: DeviceInfo.getUniqueID()
+        udid: DeviceInfo.getUniqueID(),
       },
       push_token: {
         environment: 'development',
-        client_identification_sequence: register.token
-      }
-    }
-  
-    ConnectyCube.pushnotifications.subscriptions.create(params, (error, response) => { })
+        client_identification_sequence: register.token,
+      },
+    };
+
+    ConnectyCube.pushnotifications.subscriptions.create(
+      params,
+      (error, response) => {
+        console.warn('subscriptions', error, response);
+      },
+    );
   }
 }
