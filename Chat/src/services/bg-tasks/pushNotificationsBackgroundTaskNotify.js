67b6147eb32c76ece157fc16d5e6b5b33622d355
DaveLombardo <admin@connectycube.com>
2022-05-03 15:58:39 +0300

    cleanup

--- Chat/src/services/bg-tasks/pushNotificationsBackgroundTaskNotify.js
+++
@@ -1,9 +0,0 @@
-async function pushNotificationsBackgroundTaskNotify(notificationsBundle) {
-  console.log('[pushNotificationsBackgroundTaskNotify]', notificationsBundle);
-  if (notificationsBundle.foreground) {
-    return;
-  }
-
-}
-
-module.exports = pushNotificationsBackgroundTaskNotify;
