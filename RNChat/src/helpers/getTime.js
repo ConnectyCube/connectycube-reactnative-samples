018dda49bfda0d27523beed81f1861a8b7e96ec9
DaveLombardo <admin@connectycube.com>
2022-05-03 15:35:27 +0300

    deleted old chat sample

--- RNChat/src/helpers/getTime.js
+++
@@ -1,6 +0,0 @@
-export function getTime(dateSent) {
-    const date = dateSent ? new Date(dateSent * 1000) : new Date()
-    const hours = date.getHours()
-    const minutes = date.getMinutes()
-    return `${(hours>9)?hours:('0'+hours)}:${(minutes>9)?minutes:('0'+minutes)}`
-  }
